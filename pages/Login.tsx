cb5dbe0464ef0f1ea315813a93b1c4fd1960757c
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-03-02 16:39:19 +0100

    Add login

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,18 +1,93 @@
+import Head from 'next/head';
+import Layout from '../components/Layout';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { RegisterResponseBody } from './api/register';
+
+const errorStyles = css`
+  color: red;
+`;
+
+type Errors = { message: string }[];
+
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<Errors>([]);
+
+  const router = useRouter();
+
   return (
-    <div>
-      <form onSubmit={(event) => event.preventDefault()}>
-        <label>
-          Username
-          <input></input>
-        </label>
-        <br />
-        <label>
-          Password
-          <input type="password"></input>
-        </label>
-        <button>Log-in</button>
+    <Layout>
+      <Head>
+        <title>Login at Draku</title>
+        <meta name="description" content="Draku log in" />
+      </Head>
+      <h1>Log in</h1>
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+          const loginResponse = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: username,
+              password: password,
+            }),
+          }).catch((error) => console.error(error));
+
+          const loginResponseBody =
+            (await loginResponse.json()) as RegisterResponseBody;
+
+          if ('errors' in loginResponseBody) {
+            setErrors(loginResponseBody.errors);
+            return;
+          }
+          const returnTo = router.query.returnTo;
+
+          if (
+            returnTo &&
+            !Array.isArray(returnTo) &&
+            /^\/[a-zA-Z0-9-?=]*$/.test(returnTo)
+          ) {
+            await router.push(returnTo);
+            return;
+          }
+
+          const userProfileUrl = `/users-overview/${loginResponseBody.user.id}`;
+          // Clear the errors when login worked
+          setErrors([]);
+          await router.push(userProfileUrl);
+        }}
+      >
+        <div>
+          <label>
+            Username
+            <input
+              value={username}
+              onChange={(event) => setUsername(event.currentTarget.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.currentTarget.value)}
+            />
+          </label>
+          <button>Sign-up</button>
+        </div>
       </form>
-    </div>
+      <div css={errorStyles}>
+        {errors.map((error) => {
+          return <div key={`error-${error.message}`}>{error.message}</div>;
+        })}
+      </div>
+    </Layout>
   );
 }
---
+++ pages/api/login.ts
@@ -0,0 +1,81 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcrypt';
+import { getUserWithPasswordHashByUsername, User } from '../../util/database';
+
+type LoginRequestBody = {
+  username: string;
+  password: string;
+};
+
+type LoginNextApiRequest = Omit<NextApiRequest, 'body'> & {
+  body: LoginRequestBody;
+};
+
+export type LoginResponseBody =
+  | { errors: { message: string }[] }
+  | { user: Pick<User, 'id'> };
+
+export default async function loginHandler(
+  request: LoginNextApiRequest,
+  response: NextApiResponse<LoginResponseBody>,
+) {
+  if (request.method === 'POST') {
+    if (
+      typeof request.body.username !== 'string' ||
+      !request.body.username ||
+      typeof request.body.password !== 'string' ||
+      !request.body.password
+    ) {
+      response.status(400).json({
+        errors: [
+          {
+            message: 'Username or password not provided',
+          },
+        ],
+      });
+      return;
+    }
+
+    const userWithPasswordHash = await getUserWithPasswordHashByUsername(
+      request.body.username,
+    );
+
+    if (!userWithPasswordHash) {
+      response.status(401).json({
+        errors: [
+          {
+            message: 'Username or password does not match',
+          },
+        ],
+      });
+      return;
+    }
+
+    const passwordMatches = await bcrypt.compare(
+      request.body.password,
+      userWithPasswordHash.passwordHash,
+    );
+
+    if (!passwordMatches) {
+      response.status(401).json({
+        errors: [
+          {
+            message: 'Username or password does not match',
+          },
+        ],
+      });
+      return;
+    }
+
+    // TODO: return created session
+    response.status(201).json({ user: { id: userWithPasswordHash.id } });
+    return;
+  }
+  response.status(405).json({
+    errors: [
+      {
+        message: 'Method not supported, try POST',
+      },
+    ],
+  });
+}
