9a75b7d0fc6f48fb08bc6a379ce240b5f81df663
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-03-03 16:41:26 +0100

    Add expenses and categories table with migrations

---
+++ migrations/1646320927-createTableCategories.js
@@ -0,0 +1,17 @@
+exports.up = async (sql) => {
+  await sql`
+  CREATE TABLE categories (
+		id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		user_id integer REFERENCES users (id) ON DELETE CASCADE,
+		category_name varchar(30) NOT NULL,
+		monthly_budget integer NOT NULL,
+    creation_date timestamp NOT NULL DEFAULT NOW()
+	);
+	`;
+};
+
+exports.down = async (sql) => {
+  await sql`
+    DROP TABLE categories
+  `;
+};
---
+++ migrations/1646321294-createTableExpenses.js
@@ -0,0 +1,18 @@
+exports.up = async (sql) => {
+  await sql`
+  CREATE TABLE expenses (
+		id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		user_id integer REFERENCES users (id) ON DELETE CASCADE,
+		category_id integer REFERENCES categories (id) ON DELETE CASCADE,
+		expense_name varchar(40) NOT NULL,
+		price integer NOT NULL,
+    expense_date date NOT NULL
+	);
+	`;
+};
+
+exports.down = async (sql) => {
+  await sql`
+    DROP TABLE categories
+  `;
+};
