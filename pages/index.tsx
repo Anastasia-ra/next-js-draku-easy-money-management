b5f64477f9ecf18864506cdc692857654f719577
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-03-08 20:00:12 +0100

    Update charts data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,24 @@ import { css } from '@emotion/react';
 import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import {
-  // DoughnutCategories,
+  DoughnutCategories,
   DoughnutProgress,
   LineChart,
   ProgressBar,
 } from '../components/Chart';
-import { DoughnutCategories } from '../components/charts/doughnutChart';
 import Layout from '../components/Layout';
-import { getUserByValidSessionToken } from '../util/database';
+import {
+  getCategoriesList,
+  getExpensesList,
+} from '../graph-functions/fetchApi';
+import {
+  getSharePerCategory,
+  getSumExpensesCategory,
+} from '../graph-functions/sum-per-category';
+import { getBudgetProgress } from '../graph-functions/budgetProgress';
+import { getUserByValidSessionToken, Expense } from '../util/database';
 
 const chartDoughnutStyle = css`
   width: 300px;
@@ -27,6 +36,59 @@ type Props =
   | { userObject: { username: string }; error: string };
 
 export default function Home(props: Props) {
+  const [expensesList, setExpensesList] = useState([]);
+  const [categoriesList, setCategoriesList] = useState([]);
+
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Dec',
+  ];
+
+  async function getAllExpenses(userId: number) {
+    const expensesListResponseBody = await getExpensesList(userId);
+
+    const expensesWithMonths = expensesListResponseBody.expensesList.map(
+      (e: Expense) => {
+        const expenseMonth = new Date(e.date).getMonth();
+        return {
+          ...e,
+          numericMonth: expenseMonth + 1,
+          month: months[expenseMonth],
+        };
+      },
+    );
+    console.log('expensesWithMonths', expensesWithMonths);
+
+    setExpensesList(expensesWithMonths);
+  }
+  async function getAllCategories(userId: number) {
+    const categoriesListResponseBody = await getCategoriesList(userId);
+    setCategoriesList(categoriesListResponseBody.categoriesList);
+  }
+
+  useEffect(() => {
+    if (!('error' in props)) {
+      const fetchExpenses = async () => await getAllExpenses(props.user.id);
+      fetchExpenses().catch(console.error);
+    }
+  }, [props]);
+
+  useEffect(() => {
+    if (!('error' in props)) {
+      const fetchCategories = async () => await getAllCategories(props.user.id);
+      fetchCategories().catch(console.error);
+    }
+  }, [props]);
+
   if ('error' in props) {
     return (
       <Layout userObject={props.userObject}>
@@ -46,6 +108,80 @@ export default function Home(props: Props) {
     );
   }
 
+  // Data for categories doughnut chart
+  const categoriesWithSum = getSumExpensesCategory(
+    categoriesList,
+    expensesList,
+  );
+  const categoriesWithShares = getSharePerCategory(categoriesWithSum);
+  console.log('categoriesWithShares', categoriesWithShares);
+
+  const categoriesWithSharesFiltered = categoriesWithShares.filter(
+    (e) => e.shareOfExpenses !== 0,
+  );
+
+  console.log('categoriesWithSharesFiltered', categoriesWithSharesFiltered);
+
+  const categories = categoriesWithSharesFiltered.map((e) => e.name);
+
+  const categoriesData = categoriesWithSharesFiltered.map(
+    (e) => e.shareOfExpenses,
+  );
+
+  // function setDoughnutData() {
+  //   dataDoughnutCategories.labels = categories;
+  //   dataDoughnutCategories.datasets[0].data = categoriesData;
+  //   return dataDoughnutCategories;
+  // }
+
+  const dataDoughnutCategories = {
+    labels: categories,
+    datasets: [
+      {
+        label: 'Expenses per category',
+        data: categoriesData,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // Data for progress chart
+
+  const budgetPercentage = getBudgetProgress(categoriesList, expensesList);
+
+  const dataProgressCircle = {
+    labels: ['Expenses', 'Budget left'],
+    datasets: [
+      {
+        label: 'Budget',
+        data: [1 - budgetPercentage, budgetPercentage],
+        backgroundColor: ['white', 'grey'],
+        borderColor: ['grey', 'grey'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // Data for expenses overview
+
+  console.log('expensesList', expensesList);
+
   return (
     <Layout userObject={props.userObject}>
       <Head>
@@ -58,16 +194,20 @@ export default function Home(props: Props) {
         <a>Start saving here</a>
       </Link>
       <br />
-
       <Link href="/users/categoriesManagement">
         <a>Manage your categories</a>
       </Link>
       <br />
+      <Link href="/users/budgetManagement">
+        <a>Manage your budget</a>
+      </Link>
+      <br />
+      <p>Doughnut Total</p>
       <div css={chartDoughnutStyle}>
-        <DoughnutCategories />
+        <DoughnutCategories data={dataDoughnutCategories} />
       </div>
       <div css={chartDoughnutStyle}>
-        <DoughnutProgress />
+        <DoughnutProgress data={dataProgressCircle} />
       </div>
       <div css={chartLineStyle}>
         <LineChart />
