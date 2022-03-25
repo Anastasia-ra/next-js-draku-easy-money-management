3bf28f3ec86e252985d0613f67d47d0ffd178a95
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-03-25 16:58:33 +0100

    Add config for jest puppeteer

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  launch: {
+    // Comment this in if you want to see the browser
+    headless: true,
+  },
+  browserContext: 'default',
+  server: {
+    command: 'yarn dev',
+  },
+};
