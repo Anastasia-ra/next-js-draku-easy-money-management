5a2bd7bee0512a1bfdb4e7e529a52df189e0b33f
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-03-29 15:44:25 +0200

    Delete config PWA to debug Heroku

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
-// const withPWA = require('next-pwa');
 
 const nextConfig = {
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
-
-// module.exports = withPWA({
-//   pwa: {
-//     dest: 'public',
-//     register: true,
-//     skipWaiting: true,
-//   },
-// });
