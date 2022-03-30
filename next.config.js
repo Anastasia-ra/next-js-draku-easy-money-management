0d163d79a1da3a2cc891545a0b02ceb47197b9b7
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-03-30 15:24:24 +0200

    Delete pwa config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa');
+// const runtimeCaching = require('next-pwa/cache');
 
 const nextConfig = {
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     skipWaiting: true,
+//     runtimeCaching,
+//     buildExcludes: [/middleware-manifest.json$/],
+//   },
+// });
