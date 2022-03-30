54187d6cc8848524203b1f6b9d5e5bbd6c44faed
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-03-30 15:23:09 +0200

    Set up API route for sending emails

--- pages/api/reminderEmails.js
+++ pages/api/reminderEmails.js
@@ -1,23 +1,26 @@
 import { config } from 'dotenv-safe';
 import { google } from 'googleapis';
 import nodemailer from 'nodemailer';
+import cron from 'node-cron';
 
 config();
 
 export default async function emailHandler(request, response) {
   if (request.method === 'POST') {
     if (
-      typeof request.body.name !== 'string' ||
-      !request.body.name ||
-      typeof request.body.email !== 'string' ||
-      !request.body.email ||
-      typeof request.body.day !== 'string' ||
-      !request.body.day ||
-      typeof request.body.price !== 'string' ||
-      !request.body.price
+      typeof request.body.reminder.email !== 'string' ||
+      !request.body.reminder.email ||
+      typeof request.body.reminder.name !== 'string' ||
+      !request.body.reminder.name ||
+      typeof request.body.reminder.price !== 'string' ||
+      !request.body.reminder.price
     ) {
       response.status(400).json({
-        errors: [{ message: 'Wrong input' }],
+        errors: [
+          {
+            message: 'Email, name or price not provided',
+          },
+        ],
       });
       return;
     }
@@ -26,45 +29,91 @@ export default async function emailHandler(request, response) {
     const clientSecret = process.env.CLIENT_SECRET;
     const refreshToken = process.env.REFRESH_TOKEN;
 
-    const myOAuth2Client = new google.auth.OAuth2(
+    const OAuth2 = google.auth.OAuth2;
+    const myOAuth2Client = new OAuth2(
       clientId,
       clientSecret,
       'https://developers.google.com/oauthplayground',
     );
-
     myOAuth2Client.setCredentials({
       refresh_token: refreshToken,
     });
+
     const myAccessToken = myOAuth2Client.getAccessToken();
 
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
+    console.log(request.body);
 
+    const transporter = nodemailer.createTransport({
       port: 465,
+      host: 'smtp.gmail.com',
       secure: true,
       auth: {
         type: 'OAuth2',
-        clientId: clientId,
-        clientSecret: clientSecret,
+        clientId,
+        clientSecret,
       },
     });
-    // SentMessageInfo is type directly from nodemailer dependency
-    const mailData = await transporter.sendMail({
-      from: 'expensesplitterbot@gmail.com',
-      to: request.body.email,
-      subject: `Message From ${request.body.name} regarding an Expense on Splitify`,
-      text: request.body.message,
-      html: `<div>${request.body.message}</div><p>Sent from:
-      ${request.body.name}</p> Expense List: <p>${request.body.expenseList}</p> Result: <p>${request.body.result}</p> `,
+
+    const mailData = {
+      from: 'draku.money.management@gmail.com',
+      to: request.body.reminder.email,
+      subject: `Reminder to pay your bills`,
+      text: `Hey, just a quick reminder that you have to pay ${request.body.reminder.name}. The total amount is ${request.body.reminder.price}`,
+      html: `<div>
+          Hey, just a quick reminder that you have to pay
+          ${request.body.reminder.name}. The total amount is
+          ${request.body.reminder.price}
+        </div>`,
       auth: {
-        user: 'expensesplitterbot@gmail.com',
+        user: 'draku.money.management@gmail.com',
         refreshToken: refreshToken,
         accessToken: myAccessToken,
       },
+    };
+
+    const confirmationMailData = {
+      from: 'draku.money.management@gmail.com',
+      to: request.body.reminder.email,
+      subject: `Confirmation e-mail reminder set-up `,
+      text: `Hey, you successfully registered for an e-mail reminder to pay your ${request.body.reminder.name} bill.`,
+      html: `<div>
+      Hey, you successfully registered for an e-mail reminder to pay your ${request.body.reminder.name} bill.
+        </div>`,
+      auth: {
+        user: 'draku.money.management@gmail.com',
+        refreshToken: refreshToken,
+        accessToken: myAccessToken,
+      },
+    };
+
+    transporter.sendMail(confirmationMailData, function (err, info) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+      }
     });
-    response.status(200).json({ mailData: mailData });
+
+    cron.schedule(`* * * ${request.body.reminder.day} * *`, () => {
+      transporter.sendMail(mailData, function (err, info) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(info);
+        }
+      });
+    });
+
+    response.status(200).json({ message: 'it worked' });
     return;
   }
 
-  response.status(405).json({ errors: [{ message: 'Method not supported' }] });
+  response.status(405).json({
+    errors: [
+      {
+        message: 'Method not supported, try POST',
+      },
+    ],
+  });
+  return;
 }
